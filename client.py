156e543fcce9d68f70356fcb9979aec045ce7996
root <root@ip-172-31-88-90.ec2.internal>
2017-12-15 21:06:51 +0000

    initial commit for client side utility that provisions, mounts, and creates fio profiles for AWS SGW volumes

---
+++ client.py
@@ -0,0 +1,162 @@
+import subprocess
+import os
+import platform
+import multiprocessing as mp
+import time
+
+# THINGS TO DO:
+# 1) Implement logging
+
+DISK_BY_PATH = "/dev/disk/by-path"
+SYS_BLOCK = "/sys/block"
+
+class Client(object):
+    def __init__(self, args):
+        self.gname = args.gatewayname
+        self.mkfs = args.mkfs
+        self.dirprefix = args.dirprefix
+    
+    # Eventually replace with subprocess 
+    def _run_cmd(self, cmd):
+        print cmd
+        os.system(cmd)
+
+    def iqn_to_sd(self, iqn):
+       count = 0
+       while count < 10:
+           if os.path.exists(DISK_BY_PATH):
+              for item in os.listdir(DISK_BY_PATH):
+                 if iqn in item:
+                    if os.path.islink(DISK_BY_PATH + "/" + item) == False:
+                       break
+                    return os.path.basename(os.readlink(DISK_BY_PATH + "/" + item))
+           time.sleep(1)
+           count = count + 1
+           print (count)
+           if count > 9:
+              print iqn + "did not map to block device in 10s"
+              sys.exit(-1)
+
+
+    def _create_fio_template(self):
+	with open('fiotemplate.fio','w') as f:
+	   lines = ["[global]","randrepeat=0","ioengine=libaio","iodepth=16","direct=1","numjobs=4","runtime=3600","group_reporting","time_based"]
+	   for line in lines:
+	      f.write(line + '\n')
+
+    def target_login(self, iqn_list, ip):
+        for iqn in iqn_list:
+           print "IQN: " + iqn
+           print "IP ADDRS = ", ip
+           cmd = "iscsiadm -m node -T " + iqn + " --portal " + ip + " --op=new"
+           cmd = cmd + " > /dev/null 2>&1"
+           print cmd
+           subprocess.check_output(cmd, shell=True)
+           cmd = "iscsiadm -m node -T " + iqn + " --portal " + ip + " -l"
+           print cmd
+           subprocess.check_output(cmd, shell=True)
+   
+    def target_logout_and_node_cleanup(self, targets):
+        for item in targets['CachediSCSIVolumes']:
+           targetARN = item['VolumeiSCSIAttributes']['TargetARN']
+           if self.gname in targetARN:
+              targetiqn = targetARN.split('/')[-1]
+              self._run_cmd("iscsiadm -m node -u -T %s" % targetiqn)
+              self._run_cmd("iscsiadm -m node -T %s --op=delete" % targetiqn)
+              self._run_cmd("iscsiadm -m discoverydb -t st -p %s:3260 --op=delete" % item['VolumeiSCSIAttributes']['NetworkInterfaceId'])
+           
+        self._run_cmd("iscsiadm -m session --rescan")
+        self.restart_services()
+  
+    def unmount(self):
+        cmd = "mount |grep %s | awk '{print $3}'" % self.gname
+        for l in os.popen(cmd).readlines():
+            line = l.rstrip()
+            if line == "/":
+               print "skipping unmount of /"
+               return None
+            p = os.path.basename(line)
+            print p
+            print line
+            self._run_cmd("umount %s" % line)
+            self._run_cmd("rm -rf %s" % line)
+        # cleanup fio files
+        self._run_cmd("rm -rf " + self.gname + "*.fio")
+
+    # need to figure out how to handle client_cluster_map
+    def mp_mkfs(self, mntmap):
+        pool = mp.Pool(processes=10)
+        pool.map(mkfs, mntmap)
+ 
+    # need to figure out how to handle client_cluster_map
+    def create_fio_files(self, mntmap):
+        
+	fio = {self.gname + '_randread.fio':{'rw':'randread', 'blocksize':'4k'},
+	       self.gname + '_seqread.fio':{'rw':'read', 'blocksize':'1m'},
+	       self.gname + '_randwrite.fio':{'rw':'randwrite', 'blocksize':'4k'},
+	       self.gname + '_seqwrite.fio':{'rw':'write', 'blocksize':'1m'},
+	       self.gname + '_randreadwrite.fio':{'rw':'randrw', 'rwmixread': '70', 'blocksize':'4k'}
+	      }
+
+	self._create_fio_template()
+
+	for key,item in fio.items():
+	   with open('fiotemplate.fio', 'r') as f:
+	      with open(key, 'w') as key:
+		 for line in f:
+		    key.write(line)
+		 for param in item:
+		    key.write(param + "=" + item[param] + '\n')
+		 if self.mkfs:
+		    for index in range(len(mntmap)):
+		       key.write("[fiofile]" + '\n')
+		       key.write("directory=/" + mntmap[index][1] + '\n')
+		       key.write("size=500M" + '\n')
+		 else:
+                     for item in mntmap['CachediSCSIVolumes']:
+                        targetARN = item['VolumeiSCSIAttributes']['TargetARN']
+                        if self.gname in targetARN:
+                            targetiqn = targetARN.split('/')[-1]
+                            key.write("[fiofile]" + '\n')
+                            sd = self.iqn_to_sd(targetiqn)
+                            key.write("filename=/dev/" + str(sd) + '\n')
+                            key.write("size=500M" + '\n')
+
+    def restart_services(self):
+        pass
+
+class UbuntuClient(Client):
+    def __init__(self, args):
+        super(UbuntuClient, self).__init__(args)
+        self.os_type = platform.dist()[0]
+    def restart_services(self):
+        self._run_cmd("service multipath-tools reload")
+    
+class CentosClient(Client):
+    def __init__(self, args):
+        super(UbuntuClient, self).__init__(args)
+        self.os_type = platform.dist()[0]
+    def restart_services(self):
+        self._run_cmd("service multipathd reload")
+
+# helper method for client factory
+def get_client(args):
+    if platform.dist()[0] == "Ubuntu":
+        client = UbuntuClient(args)
+    elif platform.dist()[0] == "centos":
+        client = CentosClient(args)
+    else:
+        raise ValueError("Client not supported")
+   
+    return client
+
+#Creating as a helper method because otherwise mp library does not like it if called as in instance method
+def mkfs(item):
+    if item[2].fstype == "ext4":
+	cmd = "mkfs.ext4 -E lazy_itable_init=1 {} ; mkdir -p /{}; mount {} /{}".format(item[0], item[1], item[0], item[1])
+    else:
+	cmd = "mkfs.xfs {} ; mkdir -p /{}; mount {} /{}".format(item[0], item[1], item[0], item[1])
+    print cmd
+    os.system(cmd)
+    if item[2].chown:
+	os.system("chown -R {} /{}".format(item[2].chown, item[1]))
---
+++ server.py
@@ -0,0 +1,97 @@
+import os
+import sys
+import boto3
+import botocore
+import uuid
+
+class Server(object):
+    def __init__(self, args):
+        self.gname = args.gatewayname
+        self.size = args.size
+        self.count = args.count
+        self.sgwclient = self.get_api()
+        self.gatewayARN = self.get_gateway_arn()
+
+    def _run_cmd(self, cmd):
+        print cmd
+        os.system(cmd)
+    
+
+    def get_api(self):
+	try:
+	    print "Connecting to storagegateway client using boto3 AWS SDK ..."
+            sgwclient = boto3.client("storagegateway")
+	except botocore.exceptions.NoRegionError:
+            e = "No region configured. Either export the AWS_DEFAULT_REGION environment variable or set the region value in your ~/.aws/config file by running aws config."
+            print (e)
+	    sys.exit(1)
+
+	return sgwclient
+    
+    def get_gateway_type(self):
+       gateway_type = self.sgwclient.describe_gateway_information(GatewayARN=self.gatewayARN)['GatewayType']
+       return gateway_type
+
+    def get_gateway_arn(self): 
+       response = self.sgwclient.list_gateways(Limit=10)
+       for i in response['Gateways']:
+          if i['GatewayName'] == self.gname:
+             gatewayARN = i['GatewayARN']
+             return gatewayARN
+          else: 
+             continue
+       print (self.gname + " does not exist. Please specify a valid gateway")
+       sys.exit(1)
+
+    def _get_volume_list(self):
+       volume_list = []
+       r = self.sgwclient.list_volumes(GatewayARN=self.gatewayARN)
+       for item in r['VolumeInfos']:
+          volume_list.append(item['VolumeARN'])
+       return volume_list
+
+    def get_target_iqn_list(self):
+       volume_list = self._get_volume_list()
+       iqn_list = []
+       r = self.sgwclient.describe_cached_iscsi_volumes(VolumeARNs=volume_list)
+       return r
+     
+    def get_network_interface_id(self):
+       ip_address = self.sgwclient.describe_gateway_information(GatewayARN=self.gatewayARN)["GatewayNetworkInterfaces"][0]["Ipv4Address"]
+       return ip_address
+    
+    def create_cached_iscsi_volume(self):
+       ip_address = self.get_network_interface_id()
+       size = int(self.size)*1024*1024*1024
+       
+       iqn_list = []
+       for i in range(1, self.count + 1):
+          targetname = self.gname + "-target-" + str(i)
+          clientToken = str(uuid.uuid4())
+          r = self.sgwclient.create_cached_iscsi_volume(GatewayARN=self.gatewayARN, VolumeSizeInBytes=size, TargetName=targetname, NetworkInterfaceId=ip_address, ClientToken=clientToken) 
+
+          target_iqn_list = r['TargetARN'].split("/")[-1]
+          iqn_list.append(target_iqn_list)
+
+       return iqn_list
+
+    def create_nfs_file_share(self):
+       pass
+
+    def delete_volumes(self):
+       volume_list = self._get_volume_list()
+       for vol in volume_list:
+          # add check to only delete those volumes whose target has gatewayname
+          print("Deleting volume " + vol)
+          self.sgwclient.delete_volume(VolumeARN = vol)
+         
+    def op_state_poller(self, instance):
+	count = 0
+	while count < 10:
+	   if instance['op_state'] != 'available':
+	      time.sleep(1)
+	      count = count + 1
+	   if count > 9:
+	      print instance['name'] + " did not become available in 10s"
+	      sys.exit(-1)
+	   break   
